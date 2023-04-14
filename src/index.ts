f7ab3c763da70b6e7fb8f18cbce9bda13ef19d0a
Jofran Camargo <camargojofran@gmail.com>
2023-04-14 10:44:47 -0500

    se sube archivo index.ts con la funcion click del formulario en typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-function crearalerta() {
-    alert("Hola mundo")
-}
+const form = document.getElementById("form") as HTMLFormElement;
+const btnGuardar = document.getElementById("btn-guardar") as HTMLButtonElement; 
+
+btnGuardar.addEventListener('click', (e) => {
+    // e.preventDefault();
+    console.log("ingrese a la validacion del formulario");
+
+    const tipoDocumento = document.getElementById("tipoDocumento") as HTMLInputElement;
+    const numeroDocumento = document.getElementById("numeroDocumento") as HTMLInputElement;
+    const nombre = document.getElementById("nombre") as HTMLInputElement;
+    const apellidos = document.getElementById("apellidos") as HTMLInputElement;
+    const celular = document.getElementById("celular") as HTMLInputElement;
+    const email = document.getElementById("email") as HTMLInputElement;
+    const linkedin = document.getElementById("linkedin") as HTMLInputElement;
+    const git = document.getElementById("git") as HTMLInputElement;
+
+    console.log(tipoDocumento.value);
+});
